fcb5d7a9fbd23d9f7cee1a327078d0f4f0da5ac8
Peter Hinge <contactpeterhinge@gmail.com>
2019-01-08 00:42:49 +0800

    Added KMeans model

--- Handwriting_Recognizer.py
+++ Handwriting_Recognizer.py
@@ -1,8 +1,13 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import datasets
+from sklearn.cluster import KMeans
 
+# scikit-learn training dataset of handwritten digits of 30 Turkish men from 1998:
 digits = datasets.load_digits()
 
-print(digits.DESCR)
-print(digits.data)
+# Creating the clustering unsupervised model - number of clusters is set to 10 (digits 0-9):
+model = KMeans(n_clusters=10, random_state=1)
+
+# Fitting the data to the model:
+model.fit(digits.data)
